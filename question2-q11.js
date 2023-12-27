6641b69505b3d5c1c670e5429bed225dae81bef9
solmaz-safilkani <solmaz.safikhani2004@gmail.com>
2023-12-27 14:38:46 +0330

    add Question2

---
+++ question2-q11.js
@@ -0,0 +1,18 @@
+// ? 11. Write a function called displayName() that takes an object as an argument
+// todo1 : Print the person's first and last name.
+// todo2 : Use object destructuring in the function argument.
+// todo3 : Use  template strings when printing the first and last name.
+
+const person2 = {
+    first: 'Elon',
+    last: 'Musk',
+    twitter: '@elonmusk',
+    company: 'Space X',
+  };
+  
+  // ! Answer
+  const displayName = ({ first, last }) => {
+    console.log(`Name: ${first} ${last}`);
+  };
+  
+  displayName(person2);
---
+++ question2-q6.js
@@ -0,0 +1,19 @@
+// ? 6. We have an object called 'user'.
+// Write the destructuring assignment that converts:
+// todo1 : 'name' property into the variable 'firstName'.
+// todo2 : 'years' property into the variable 'age'.
+// todo3 : 'admin' property into the variable 'isAdmin'.(if no such property exists create 'isAdmin' variable and assign false value to that)
+
+let user = { name: 'John', years: 30 };
+
+// console.log(name); // John
+// console.log(age); // 30
+// console.log(isAdmin); // false
+
+// ! Answer
+
+let { name: firstName, years: age, admin: isAdmin = false } = user;
+
+console.log(firstName); 
+console.log(age); 
+console.log(isAdmin); 
